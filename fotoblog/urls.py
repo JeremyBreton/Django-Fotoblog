900f0e01642a6ac68ddfff068b2d2d8e8a978697
Jérémy <breton.jeremy@live.fr>
2024-09-11 15:29:09 +0200

    add blog write and view

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -24,8 +24,12 @@ urlpatterns = [
          name='password_change_done'
          ),
     path('signup/', authentication.views.signup_page, name='signup'),
+    path('profile-photo/upload', authentication.views.upload_profile_photo,
+         name='upload_profile_photo'),
     path('home/', blog.views.home, name='home'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
+    path('blog/create', blog.views.blog_and_photo_upload, name='blog_create'),
+    path('blog/<int:blog_id>', blog.views.view_blog, name='view_blog'),
 ]
 
 if settings.DEBUG:
