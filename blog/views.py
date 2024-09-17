165c9be572f34aead6ece613d17c7e70334738d7
Jérémy <breton.jeremy@live.fr>
2024-09-17 11:38:53 +0200

    display blogs_and_photos in /home

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from itertools import chain
+
 from django.contrib.auth.decorators import login_required, permission_required
 from django.forms import formset_factory
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from . import forms, models
@@ -21,11 +24,23 @@ def photo_upload(request):
     return render(request, 'blog/photo_upload.html', context={'form': form})
 
 
-@login_required
 def home(request):
-    photos = models.Photo.objects.all()
-    blogs = models.Blog.objects.all()
-    return render(request, 'blog/home.html', context={'photos': photos, 'blogs': blogs})
+    blogs = models.Blog.objects.filter(
+        Q(contributors__in=request.user.follows.all()) | Q(starred=True))
+    photos = models.Photo.objects.filter(
+        uploader__in=request.user.follows.all()).exclude(
+        blog__in=blogs)
+
+    blogs_and_photos = sorted(
+        chain(blogs, photos),
+        key=lambda instance: instance.date_created,
+        reverse=True
+    )
+
+    context = {
+        'blogs_and_photos': blogs_and_photos,
+    }
+    return render(request, 'blog/home.html', context=context)
 
 
 @login_required
@@ -108,3 +123,12 @@ def follow_users(request):
             form.save()
             return redirect('home')
     return render(request, 'blog/follow_users_form.html', context={'form': form})
+
+
+def photo_feed(request):
+    photos = models.Photo.objects.filter(
+        uploader__in=request.user.follows.all()).order_by('-date_created')
+    context = {
+        'photos': photos,
+    }
+    return render(request, 'blog/photo_feed.html', context=context)
