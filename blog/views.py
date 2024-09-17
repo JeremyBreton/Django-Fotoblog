7ab4f5719f29699bbc129d6a54bcefd664e432ad
Jérémy <breton.jeremy@live.fr>
2024-09-17 11:55:04 +0200

    add pagination + end of exercise

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from itertools import chain
 
 from django.contrib.auth.decorators import login_required, permission_required
+from django.core.paginator import Paginator
 from django.forms import formset_factory
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
@@ -24,6 +25,7 @@ def photo_upload(request):
     return render(request, 'blog/photo_upload.html', context={'form': form})
 
 
+@login_required
 def home(request):
     blogs = models.Blog.objects.filter(
         Q(contributors__in=request.user.follows.all()) | Q(starred=True))
@@ -36,10 +38,11 @@ def home(request):
         key=lambda instance: instance.date_created,
         reverse=True
     )
+    paginator = Paginator(blogs_and_photos, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
 
-    context = {
-        'blogs_and_photos': blogs_and_photos,
-    }
     return render(request, 'blog/home.html', context=context)
 
 
@@ -128,7 +131,8 @@ def follow_users(request):
 def photo_feed(request):
     photos = models.Photo.objects.filter(
         uploader__in=request.user.follows.all()).order_by('-date_created')
-    context = {
-        'photos': photos,
-    }
+    paginator = Paginator(photos, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'blog/photo_feed.html', context=context)
